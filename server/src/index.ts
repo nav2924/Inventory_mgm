200a2933bef79a8a2cf67a55eff96b1743049a75
nav2924 <navi29012004@gmail.com>
2025-02-24 05:21:33 +0530

    purchase and sale summary

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from "cors";
+import bodyParser from 'body-parser';
+import dotenv from "dotenv";
+import helmet from "helmet"
+import morgan from "morgan";
+import dashboardRoutes from "./routes/dashboardRoutes"
+
+dotenv.config()
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// app.get("/hello",(req,res) => {
+//     res.send("Hello")
+//     console.log("hello")
+// })
+
+app.use("/dashboard", dashboardRoutes)
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`)
+})
---
+++ server/src/routes/dashboardRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getDashboardMetrics } from "../controller/dashboardController";
+
+
+const router = Router();
+
+router.get("/",getDashboardMetrics)
+
+export default router;
