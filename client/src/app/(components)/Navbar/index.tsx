a390c0dc98309aae55be50f491ddff4003088310
nav2924 <navi29012004@gmail.com>
2025-03-06 04:44:16 +0530

    profile

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -5,6 +5,7 @@ import { setIsDarkMode, setIsSidebarCollapsed } from "../../state";
 import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -72,7 +73,14 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <div className="w-9 h-9">Image</div>
+            <Image
+              src="https://s3-inventory-management-navi.s3.us-east-1.amazonaws.com/assets/profile.jpg"
+              width={50}
+              height={50}
+              alt="profile image"
+              className="rounded-full h-full object-cover"
+            />
+
             <div className="font-semibold">Naveen</div>
           </div>
         </div>
